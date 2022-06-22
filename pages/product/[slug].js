56c5b05c80dd9b5f03517a9eaf8039776c3f750c
Prateek Bihani <delhiopenbooks2016@gmail.com>
2022-06-22 11:11:47 +0530

    Till Cart Section

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,12 +1,14 @@
 import React , {useState} from 'react';
 import {client,urlFor } from '../../lib/Client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import {Product} from '../../components'
+import {Product} from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({product,products}) => {
-    const { image, name, details, price } = product;
+    const { image, name, details, price ,author} = product;
 
     const [index,setIndex]=useState(0);
+    const { decQty, incQty, qty,onAdd} = useStateContext();
 
   return (
     <div >
@@ -46,38 +48,33 @@ const ProductDetails = ({product,products}) => {
                     </div>
                     <h4>Details:</h4>
                     <p>{details}</p>
+                    <span><b>Author: </b>{author}</span>
                     <p className='price'> &#x20B9; {price}</p>
-                    <div className='quantity'>
-                        <h3>Quantity:</h3>
-                        <p className='quantity-desc'>
-                            <span className='minus' onClick=""><AiOutlineMinus/></span>
-                            <span className='num' onClick="">0</span>
-                            <span className='plus' onClick=""><AiOutlinePlus/></span>
-                        </p>
-
-                    </div>
-                    <div className='buttons'>
-                        <button type='button' className='add-to-cart' onClick="">Add To Cart</button>
-                        <button type='button' className='buy-now' onClick="" >Buy Now</button>
-                    </div>
-                </div>
+                    <div className="quantity">
+                    <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="buy-now" onClick="">Buy Now</button>
+          </div>
         </div>
-        <div className='maylike-products-wrapper'>
-            <h2>You may also like</h2>
-            <div className="marquee">
-                <div className="maylike-products-container track">
-                    {
-                        products.map((item)=>(
-                            <Product key={item._id}
-                            product={item}/>
-                        ))
-                    }
-                    
-                </div>
+      </div>
 
+      <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
             </div>
-
-        </div>
+          </div>
+      </div>
     </div>
   )
 }
